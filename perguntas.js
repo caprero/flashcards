0bdd682c0679599fa569752dea687c12b35c66c5
caprero <135332551+caprero@users.noreply.github.com>
2024-10-27 21:46:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,46 @@
+criaCartao(
+    'Até o verão terminar.',
+    'Como o relacionamento entre Beyah e Samson começa?',
+    'O relacionamento entre Beyah e Samson começa quando eles se conhecem na praia, logo após Beyah se mudar para a casa de seu pai para passar o verão. Ambos são atraídos um pelo outro por causa de suas histórias difíceis e acabam desenvolvendo uma conexão profunda e instantânea.'
+)
+
+
+criaCartao(
+    'Até o verão terminar.',
+    'Por que Beyah tem dificuldades em confiar nas pessoas?',
+    'Beyah tem dificuldades em confiar nas pessoas porque viveu uma infância marcada pela negligência e pelos problemas com a mãe, que era viciada. Ela cresceu precisando cuidar de si mesma e acostumou-se a não depender de ninguém, o que a deixou desconfiada de quem se aproxima.'
+)
+
+criaCartao(
+    'Até o verão terminar.',
+    'Como o passado de Samson influencia sua personalidade e suas escolhas?',
+    'Samson teve uma infância complicada, o que o tornou reservado e o fez se refugiar em sua própria dor. Ele esconde muitos segredos, incluindo aspectos importantes de sua vida, o que o torna misterioso, e essa dificuldade de se abrir afeta sua relação com Beyah.'
+)
+
+
+criaCartao(
+    'Até o verão terminar.',
+    'Qual é o papel das amizades de Beyah ao longo do livro?',
+    'As amizades que Beyah faz com Sara, sua meia-irmã, e Marcos, namorado de Sara, ajudam-na a se abrir e se sentir mais aceita. Eles se tornam uma rede de apoio para ela, mostrando que nem todas as pessoas vão abandoná-la, e que ela pode confiar e depender de quem se importa com ela.'
+)
+
+
+criaCartao(
+    'Até o verão terminar.',
+    'Como o tema do perdão é abordado no livro?',
+    'O tema do perdão é presente principalmente nas relações familiares. Beyah precisa lidar com seus sentimentos de abandono em relação ao pai e com a raiva em relação à mãe. Ao longo do livro, ela começa a entender melhor o passado deles e, gradualmente, aprende a perdoar.'
+)
+
+
+criaCartao(
+    'Até o verão terminar.',
+    'Que lições Beyah aprende ao longo da história?',
+    'Beyah aprende a importância de confiar nas pessoas e percebe que não precisa enfrentar todos os desafios sozinha. Ela também entende que, apesar das dificuldades, ela pode abrir seu coração e encontrar felicidade e aceitação ao lado de pessoas que a amam.'
+)
+
+
+criaCartao(
+    'Até o verão terminar.',
+    'Como o final do livro afeta o desenvolvimento emocional de Beyah?',
+    'O final do livro é marcante, pois Samson precisa lidar com as consequências de seus segredos. Isso impacta Beyah, mas também a fortalece, ensinando-lhe que o amor verdadeiro inclui aceitar o outro com suas falhas e que, apesar da dor, ela pode seguir em frente com o que aprendeu.'
+)
